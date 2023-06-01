24c191af5b0d4a958285e03c35fecc33362dcebc
4L302 <alegarg2002@gmail.com>
2023-06-01 16:59:05 +0200

    agg migrate con faker

--- database/migrations/2023_05_31_144029_create_trains_table.php
+++ database/migrations/2023_05_31_144029_create_trains_table.php
@@ -16,13 +16,13 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('Azienda');
-            $table->string('Stazione di partenza');
-            $table->string('Stazione di arrivo');
-            $table->time('Orario di partenza');
-            $table->time('Orario di arrivo');
-            $table->string('Codice Treno');
-            $table->integer('Numero Carrozze');
-            $table->boolean('In orario');
+            $table->string('Stazione_di_partenza');
+            $table->string('Stazione_di_arrivo');
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->string('Codice_Treno');
+            $table->integer('Numero_Carrozze');
+            $table->boolean('In_orario');
             $table->boolean('Cancellato');
             $table->timestamps();
             
--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+use App\Models\Trains;
+
 class TrainTableSeeder extends Seeder
 {
     /**
@@ -12,25 +15,20 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $data = [
-            [
-                'Azienda' => '',
-                'Stazione di partenza' => '',
-                'Stazione di arrivo' => '',
-                'Orario di partenza' => '',
-                'Orario di arrivo' => '',
-                'Codice Treno' => '',
-                'Numero Carrozze' => '',
-                'In orario' => '',
-                'Cancellato' => ''
-            ],
-        ];
-        foreach($data as $trains){
-        $newTrain = new TrainTableSeeder();
-        $newTrain->Azienda = $trains['Azienda'];
-        $newTrain->save();
+        for($i = 0; $i < 20; $i ++){
+            $newTrain = new Train();
+            $newTrain -> Azienda = '';
+            $newTrain -> Stazione_di_partenza = '';
+            $newTrain -> Stazione_di_arrivo = '';
+            $newTrain -> Orario_di_partenza = '';
+            $newTrain -> Orario_di_arrivo = '';
+            $newTrain -> Codice_Treno = '';
+            $newTrain -> Numero_Carrozze = '';
+            $newTrain -> In_orario = '';
+            $newTrain -> Cancellato = '';
+            $newTrain->save();
         }
     }
 }
