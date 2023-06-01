3359740db71dfef1e6fe9f46798ebe202acc4fac
4L302 <alegarg2002@gmail.com>
2023-06-01 16:02:43 +0200

    .

--- database/migrations/2023_05_31_144029_create_trains_table.php
+++ database/migrations/2023_05_31_144029_create_trains_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('Azienda');
-            $table->longText('Stazione di partenza');
-            $table->longText('Stazione di arrivo');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
             $table->time('Orario di partenza');
             $table->time('Orario di arrivo');
             $table->string('Codice Treno');
