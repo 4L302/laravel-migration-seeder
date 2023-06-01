476cb4bbfcd73d46274527e6fd74f31b5f635494
4L302 <alegarg2002@gmail.com>
2023-06-01 17:28:50 +0200

    .

--- database/seeders/TrainTableSeeder.php
+++ database/seeders/TrainTableSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use Faker\Generator as Faker;
-use App\Models\Trains;
+use App\Models\Train;
 
 class TrainTableSeeder extends Seeder
 {
