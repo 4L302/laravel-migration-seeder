0db7c2b35c55303b9349c5e3dd7f976ccc960e6e
4L302 <alegarg2002@gmail.com>
2023-06-01 17:41:08 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,13 @@
 
 <main class="container text-center justify-content-center py-5 text-uppercase">
     <h1>Laravel Migration Seeder</h1>
+    <div>
+        <ul>
+        @foreach ($trains as $train)
+            <li>{{ $train->Azienda }}</li>
+        @endforeach
+    </ul>
+    </div>
 </main>
 
 @endsection
